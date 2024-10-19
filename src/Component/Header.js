03cc489ebb000025b5af23e171e8412824e43e39
unknown <raj.sah.2209@gmail.com>
2024-10-19 15:29:33 +0530

    changed background

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div style={{ width: '100%', height: '40vh' }}>
-            <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src="/sahibganjdcoffice.jpg" alt="Sahibganj DC Office" />
+        <div style={{ width: '100%', height: '60vh' }}>
+            <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src="/new_background.png" alt="Sahibganj DC Office" />
         </div>
     );
 };
