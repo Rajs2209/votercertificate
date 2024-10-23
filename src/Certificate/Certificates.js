3e9189f14e7d87ba1407fe2be9ae14c22b58a550
unknown <raj.sah.2209@gmail.com>
2024-10-23 10:22:55 +0530

    changed vidhan sabha name and image

--- src/Certificate/Certificates.js
+++ src/Certificate/Certificates.js
@@ -10,7 +10,7 @@ const Certificates = () => {
 
     useEffect(() => {
         const img = new Image();
-        img.src = '/sbgnewimage.png';
+        img.src = '/final.png';
         img.onload = () => {
             setImageLoaded(true);
         };
@@ -22,7 +22,7 @@ const Certificates = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         const img = new Image();
-        img.src = '/sbgnewimage.png';
+        img.src = '/final.png';
 
         canvas.width = img.width;
         canvas.height = img.height;
--- src/Component/Formuser.js
+++ src/Component/Formuser.js
@@ -114,9 +114,9 @@ const Formuser = () => {
                             <div>
                                 <select id="inputState" className="my-2 p-2 w-75" name="option" onChange={handleInput} value={formData.option}>
                                     <option value="" disabled>--विधानसभा क्षेत्र--</option>
-                                    <option value="राजमहल">राजमहल</option>
-                                    <option value="बोरियों">बोरियों</option>
-                                    <option value="बरहेट">बरहेट</option>
+                                    <option value="राजमहल">01 - राजमहल </option>
+                                    <option value="बोरियों">02 - बोरियो (अ०ज०जा०)</option>
+                                    <option value="बरहेट">03 - बरहेट (अ०ज०जा०)</option>
                                 </select>
                             </div>
 
